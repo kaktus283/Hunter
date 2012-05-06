b7db9c51676f6dc8ccf4dcd8971ce414d9818fe8
Bartek Wiórkiewicz <kaktus283@gmail.com>
2012-05-06 22:31:19 +0200

    0.3.2 - new background, fix slowing mosnter, mozilla support

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,12 @@
 var a = 0;
 var b = 0;
-var version = 'Developer Preview 0.3.1';
+var version = 'Developer Preview 0.3.2';
 function addMonster (value) {
   for(var i = 1; i <= value; i++) {
     monster = document.createElement('div');
     monster.id = a++;
     monster.setAttribute('onclick', 'remove('+b+++')');
-    monster.setAttribute('class', 'squirrel');
+    monster.setAttribute('class', 'husky');
     //Need rewrite
     monster.style.left = Math.floor( ( Math.random( ) * 95 ) + 1) +"%";
     monster.style.top = 5 + Math.floor( ( Math.random( ) * 5 ) + 1 ) + "%";
@@ -26,7 +26,7 @@ function addPoints ( ) {
   points = score + 1;
   if(points == 10 || points == 20 || points == 30) {
     addMonster(1);
-//    document.getElementById('level').innerHTML = '2';
+    //document.getElementById('level').innerHTML = '2';
   }
   document.getElementById('points').innerHTML = points;
 }
